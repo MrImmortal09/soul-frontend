9e9486ebc561384a6b64147a0b1176fd08aa84f7
MrImmortal09 <reach.oms2004@gmail.com>
2025-01-19 07:21:32 +0530

    fk

--- app/components/sideBar/SideBar.jsx
+++ app/components/sideBar/SideBar.jsx
@@ -3,7 +3,7 @@
 
 export default function SideBar() {
   return (
-    <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-primary/20 via-background to-background relative">
+    <div className="min-h-screen customfont bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-primary/20 via-background to-background relative">
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_top_right,_var(--tw-gradient-stops))] from-purple-500/20 via-transparent to-transparent"></div>
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-purple-500/20 via-transparent to-transparent"></div>
       
